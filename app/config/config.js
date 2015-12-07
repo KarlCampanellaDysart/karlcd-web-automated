9a682973b4810a81031d8f1e8238b7b96a8e0d36
Karl Campanella-Dysart <karlcd@me.com>
2015-12-06 21:58:03 -0800

    changed gitignore and Procfile for heroku deployment

---
+++ app/config/config.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+	github_token: 'token 38cafe206eb7ece89aec0ce2cda7017493245f96',
+	APP_ID: '82n18ioLKCw3fLyxCcZyVVctFOHinYlYDOeUuqpz',
+	MASTER_KEY: 'IOl3Ps15hmbrA1gW88hB43gngJsJXYWkWbqRvHTL',
+	username:'KarlCampanellaDysart',
+	login: {
+		username: 'karlcd',
+		password: 'karlcd',
+		auth_token: 'q87dsg9vshjl%$798t3jk'
+	}
+};
