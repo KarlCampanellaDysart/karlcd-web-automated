5e0fef11a540f3ca98310047ac9f2e83afec30d7
Karl Campanella-Dysart <karlcampanelladysart@Karls-MacBook-Pro.local>
2017-01-11 16:34:49 -0800

    adding config in deploy branch

---
+++ app/config/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	github_token: 'token aecdceaf2e42ba9658c2b2de201ac92fdfa33420',
+	APP_ID: '82n18ioLKCw3fLyxCcZyVVctFOHinYlYDOeUuqpz',
+	MASTER_KEY: 'IOl3Ps15hmbrA1gW88hB43gngJsJXYWkWbqRvHTL',
+	username:'KarlCampanellaDysart',
+	login: {
+		username: 'karlcd',
+		password: 'karlcd_web',
+		auth_token: 'q87dsg9vshjl%$798t3jk'
+ 	}
+};
